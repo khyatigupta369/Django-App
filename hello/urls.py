074be86350a0a69d5dac10b90670725da769e347
Khyati Gupta <khyatigupta369@gmail.com>
2023-08-20 02:07:16 +0530

    index.html added as template

--- hello/urls.py
+++ hello/urls.py
@@ -19,6 +19,7 @@ from home import views
 
 urlpatterns = [
     path("", views.index, name='home'),
-    path("", views.about, name='about'),
-    path("", views.services, name='services'),
+    path("about", views.about, name='about'),
+    path("services", views.services, name='services'),
+    path("contact", views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse("This is Homepage")
+    context = {
+        'variable': "this is a variable text",
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse("This is About")
 
 def about(request):
     return HttpResponse("This is About")
 
 def services(request):
     return HttpResponse("This is Services")
+
+def contact(request):
+    return HttpResponse("This is Contact")
